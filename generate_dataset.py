4a7a2ab84a2e5ecb0c1ae2fe5bff1bac70af10f6
Isala Piyarisi <code@isala.me>
2020-03-23 21:30:23 +0530

    Updated to generator double both vertices and edges on each step

--- generate_dataset.py
+++ generate_dataset.py
@@ -12,23 +12,40 @@ need_vertices = current_vertices
 need_edges = current_edges
 
 
-for m in range(10):
-    need_vertices *= 2
-    need_edges *= 3
+def add_edge(source_node, network_size):
+    if source_node == t:
+        return False
+    destination = randint(0, need_vertices - 2)
+    while destination == s:
+        destination = randint(0, need_vertices - 2)
 
+    capacity = randint(1, network_size)
+    if source_node == s:
+        network.add_edge(source_node, destination, capacity, source_name="S")
+    elif destination == t:
+        network.add_edge(source_node, destination, capacity, destination_name="T")
+    else:
+        network.add_edge(source_node, destination, capacity)
+    return True
+
+
+for m in range(9):
+    need_vertices *= 2
+    need_edges *= 2
+    first_iteration = True
     while current_edges < need_edges:
-        for i in range(current_vertices, need_vertices-1):
-            if i == current_vertices or randint(0, 2):
+        if first_iteration:
+            for i in range(current_vertices, need_vertices - 1):
                 current_edges += 1
-                source = i
-                destination = randint(0, need_vertices - 2)
-                capacity = randint(1, need_vertices - 2)
-                if source == s:
-                    network.add_edge(source, destination, capacity, source_name="S")
-                elif destination == t:
-                    network.add_edge(source, destination, capacity, destination_name="T")
-                else:
-                    network.add_edge(source, destination, capacity)
+                add_edge(i, need_vertices)
+            current_vertices = need_vertices
+            first_iteration = False
+        else:
+            for i in range(0, need_vertices - 1):
+                if randint(0, 2):
+                    if current_edges >= need_edges:
+                        break
+                    current_edges += 1
+                    add_edge(i, need_vertices)
 
     network.save(f"{m + 1}-vertices-{network.network_size()},edges-{current_edges}")
-
